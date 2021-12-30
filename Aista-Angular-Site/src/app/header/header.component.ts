6911cfb1ceab37fb75b63a62bd4d1fad99ec1029
Sam-Varghese <sam.varghese.sica@gmail.com>
2021-12-30 10:23:00 +0530

    Designed the form
    I've designed the form using sass. Now will be working on ticker, and some backend part of form.

--- Aista-Angular-Site/src/app/header/header.component.ts
+++ Aista-Angular-Site/src/app/header/header.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  
+  eventDate = new Date("2022 june 15");
+  currentDate = new Date();
 
-  constructor() { }
+  timeLeftInMilliSeconds = this.eventDate.getTime() - this.currentDate.getTime();
+  
+  daysLeft = this.timeLeftInMilliSeconds / (60 * 60 * 1000 * 24) + " days";
+  hoursLeft = (this.timeLeftInMilliSeconds % (60 * 60 * 1000 * 24)) * 24 + " hours";
+  minutesLeft = (this.timeLeftInMilliSeconds % (60 * 60 * 1000 * 24)) * 24 * 60 + " minutes";
+  secondsLeft = (this.timeLeftInMilliSeconds % (60 * 60 * 1000 * 24)) * 24 * 60 * 60 + " seconds";
+
+
+
+  constructor() { };
 
   ngOnInit(): void {
-  }
 
+    let updateTime = () => {
+
+      setInterval(() => {
+      let eventDate = new Date('2022 june 15');
+      let currentDate = new Date();
+
+      let timeLeftInMilliSeconds =
+        this.eventDate.getTime() - this.currentDate.getTime();
+
+      let daysLeft = this.timeLeftInMilliSeconds / (60 * 60 * 1000 * 24) + ' days';
+      let hoursLeft =
+        (this.timeLeftInMilliSeconds % (60 * 60 * 1000 * 24)) * 24 + ' hours';
+      let minutesLeft =
+        (this.timeLeftInMilliSeconds % (60 * 60 * 1000 * 24)) * 24 * 60 +
+        ' minutes';
+      let secondsLeft =
+        (this.timeLeftInMilliSeconds % (60 * 60 * 1000 * 24)) * 24 * 60 * 60 +
+        ' seconds';}, 1000) 
+    }
+    updateTime();
+  }
 }
