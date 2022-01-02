0657f826ba158e6db188fa70eafa8394f93bffa5
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-01-02 21:22:46 +0530

    Imroved site as per suggestions
    Removed .css.map files, .spec.ts files, now components have only 3 important files. I've removed .css files too, and did configurations to display css from .scss files itself. Deleted greet.js file and <app-form-page> from index.html. I've also removed all placeholders from the form

--- Aista-Angular-Site/src/app/form-page/form-page.component.ts
+++ Aista-Angular-Site/src/app/form-page/form-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-page',
   templateUrl: './form-page.component.html',
-  styleUrls: ['./form-page.component.css']
+  styleUrls: ['./form-page.component.scss']
 })
 export class FormPageComponent implements OnInit {
 
